ea9faef11158a6ab7fa1e268ffa4f1819b2d11ef
Sean Knowles <s@seanknowles.dev>
2024-05-03 08:47:35 +0100

    feat: websockets working :

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var quizzCollection *mongo.Collection
 
 func main() {
-	fmt.Println("Hello, world!")
+	app := fiber.New()
+	app.Use(cors.New())
+
+	setupMongoDB()
+
+	app.Get("/", index)
+	app.Get("/api/quizzes", getQuizzes)
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+		// Send a message to the client
+		err := c.WriteMessage(websocket.TextMessage, []byte("Hello from server!"))
+		if err != nil {
+			log.Println("Write error:", err)
+			return
+		}
+
+		// Read messages from the client
+		for {
+			_, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("Read error:", err)
+				break
+			}
+			log.Printf("Received message: %s", msg)
+		}
+	}))
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func setupMongoDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:BVFxHoSvdJEANamwrkgPtBRCigkLDJUR@viaduct.proxy.rlwy.net:53598"))
+	if err != nil {
+		panic(err)
+	}
+	quizzCollection = client.Database("test").Collection("quizzes")
+}
+
+func index(c *fiber.Ctx) error {
+	return c.SendString("Hello world")
+}
+
+func getQuizzes(c *fiber.Ctx) error {
+	cursor, err := quizzCollection.Find(context.Background(), bson.M{})
+	if err != nil {
+		panic(err)
+	}
+
+	var quizzes []map[string]interface{}
+
+	if err = cursor.All(context.Background(), &quizzes); err != nil {
+		panic(err)
+	}
+
+	return c.JSON(quizzes)
 }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,86 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import { onMount } from "svelte";
+
+let quizzes: unknown[] = []; // Change type to unknown[]
+let websocket: WebSocket;
+
+async function getQuizzes() {
+	try {
+		const resp = await fetch("http://localhost:3000/api/quizzes", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		console.log(resp);
+		if (resp.ok) {
+			quizzes = await resp.json();
+		}
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+function connect() {
+	websocket = new WebSocket("ws://localhost:3000/ws/1");
+
+	websocket.onopen = () => {
+		console.log("Connected to WebSocket");
+	};
+
+	websocket.onmessage = (event) => {
+		console.log("Received message:", event.data);
+		// Handle incoming WebSocket messages here
+	};
+
+	websocket.onclose = () => {
+		console.log("WebSocket connection closed");
+		// Optionally, you can attempt to reconnect here
+	};
+
+	websocket.onerror = (error) => {
+		console.error("WebSocket error:", error);
+	};
+}
+
+function sendMessage(message: string) {
+	if (websocket.readyState === WebSocket.OPEN) {
+		websocket.send(message);
+	} else {
+		console.error("WebSocket is not open");
+	}
+}
+
+function onMessage(event: MessageEvent) {
+	console.log("Received message:", event.data);
+	// Handle incoming WebSocket messages here
+	// For example, you can update the UI or store the received data
+}
+
+onMount(() => {
+	connect();
+
+	return () => {
+		if (websocket) {
+			websocket.close();
+		}
+	};
+});
+</script>
+
+<div>
+  <h1>Hello world!</h1>
+
+  <button on:click={getQuizzes}>Get Quizzes</button>
+  
+  <pre>
+    {#each quizzes as quiz}
+      <pre>{JSON.stringify(quiz, null, 2)}</pre>
+    {/each}
+  </pre>
+
+  <!-- Add a button to send a test socket message -->
+  <button on:click={() => sendMessage('Test message')}>Send Test Message</button>
+
+</div>
